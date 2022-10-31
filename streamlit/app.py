974af878ad188a83a66b1984a741666a261ec3a7
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-10-31 17:35:13 +0100

    Use requests for http and add language choice

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,12 +1,11 @@
-import json
-from urllib.request import urlopen
-from urllib.parse import quote_plus
-
+import requests
 import streamlit as st
 
-SOLR_URL = "http://localhost:8983/solr/#/dialogs/"
 
-def query_solr(query:str) -> str:
+SOLR_URL = "http://solr:8983/solr/dialogs/query"
+
+
+def query_solr(query: str, language: str) -> str:
     """
     Function to query the Solr database with a question
 
@@ -14,20 +13,25 @@ def query_solr(query:str) -> str:
     ----------
         query, str:
             The question to be searched
+        language, str:
+            Question language abbreviation
     """
-    if query is not None:
-        query_string = f"Q:\"{query}\""
-        query_url = SOLR_URL +"select?q=" +quote_plus(query_string)
-        with urlopen(query_url) as connection:
-            response = json.load(connection)
-            if response['response']['numFound'] == 0:
-                return None
-            return response['response']['docs'][0]['A']
-    return None
+    if query is None:
+        return None
+    r = requests.get(SOLR_URL, json={"query": f"Q_txt_{language}:{query}"})
+    if not r.ok:
+        return None
+    response = r.json()
+    docs = response["response"]["docs"]
+    if not docs:
+        return None
+    return docs[0][f"A_txt_{language}"]
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     st.title("Sparse Retrieval QA")
+    language = st.radio(label="Language", options=["PL", "EN"])
     question = st.text_input("Enter question")
     if question:
-        answer = query_solr(question)
+        answer = query_solr(question, language.lower())
         st.write(answer)
